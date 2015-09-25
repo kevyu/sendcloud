c185023613794bf19f6b070e89c1bcaae87e702f
Edward Yang <yushine999@gmail.com>
2015-09-25 11:49:58 +0800

    Added send_template API

--- lib/sendcloud/mail.rb
+++ lib/sendcloud/mail.rb
@@ -8,6 +8,10 @@ module Sendcloud
       Sendcloud.post('mail.send', params)
     end
 
+    def send_template(params = {})
+      Sendcloud.post('mail.send_template', params)
+    end
+
   end
 
 end
